25b2da5322390df864e4da69dc38b9d14a5dc84f
Lawrence Ofoli Mensah <lawrenceofoli@gmail.com>
2024-07-18 04:52:24 +0000

    login user

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const crypto = require("crypto");
 const jwt = require("jsonwebtoken");
-let books = require("./booksdb.js");
+const books = require("./booksdb.js");
 const regd_users = express.Router();
 
-let users = [];
+const JWT_SECRET = "b10a8db164e0754105b7a99be72e3fe5";
+const users = [];
+
+module.exports.Token = {
+  generate: (data) => jwt.sign(data, JWT_SECRET, { expiresIn: "1h" }),
+  verify: async (token) => await jwt.verify(token, JWT_SECRET),
+};
 
 module.exports.hashPassword = (password) =>
   crypto.createHash("md5").update(password).digest("hex");
@@ -21,8 +27,28 @@ const authenticatedUser = (username, password) => {
 
 //only registered users can login
 regd_users.post("/login", (req, res) => {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const { username, password } = req.body;
+  if (!username || !password) {
+    return res
+      .status(400)
+      .json({ status: false, message: "username and password required" });
+  }
+
+  const user = users.find(
+    (user) =>
+      user.username === username &&
+      user.password === this.hashPassword(password)
+  );
+  if (!user) {
+    return res
+      .status(400)
+      .json({ status: false, message: "Invalid username or password" });
+  }
+
+  const token = this.Token.generate({ id: user.id, username });
+  req.session.token = token;
+
+  return res.json({ status: true, user: { id: user.id, username } });
 });
 
 // Add a book review
